446349a266edfef60e9457484db9b0442c50130d
Maaz Ali <abrahamlincoln1027@gmail.com>
2025-03-16 13:48:09 +0500

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 const { defineConfig } = require('vite')
 const react = require('@vitejs/plugin-react')
+const path = require('path')
 
 // https://vite.dev/config/
 module.exports = defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
