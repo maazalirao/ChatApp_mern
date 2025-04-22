53b28ae6ca72ae1a1fee828d95279783045f39ce
maazalirao <abrahamlincoln1027@gmail.com>
2025-04-23 04:11:39 +0500

    scroll

--- src/pages/ChatRoom.jsx
+++ src/pages/ChatRoom.jsx
@@ -312,7 +312,7 @@ const ChatRoom = ({ socket, username, room, setRoom, navigate }) => {
       
       return {
         ...prevReactions,
-        [messageId]: updatedMessageReactions
+        [message]: updatedMessageReactions
       };
     });
     
